28397b0a86a9c3f8606a109f29a34d0a8c6c16dc
IrynaKIM2001 <ira2001tu@gmail.com>
2025-04-27 22:07:42 +0300

    HW1 Task_2

---
+++ Homework/Lesson-1/Task-2_6Qb97gsv/task-2.js
@@ -0,0 +1,9 @@
+let firstName = "Ірина";
+let middleName = "Михайлівна";
+let lastName = "Коваль";
+
+let msg = lastName + " " + firstName + " " + middleName;
+console.log(msg);
+
+let msg2 = `${lastName} ${firstName} ${middleName}`;
+console.log(msg2);
