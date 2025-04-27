61bca3fc7a8d0682d9db3de718896aa2b5ea8b9b
IrynaKIM2001 <ira2001tu@gmail.com>
2025-04-27 22:30:51 +0300

    HW1 Task_4

---
+++ Homework/Lesson-1/Task-4_ruUtWDUI/task-4.js
@@ -0,0 +1,6 @@
+let name = prompt("Введіть ім'я:");
+console.log("Ім'я:", name);
+let middleName = prompt("Введіть по-батькові:");
+console.log("По-батькові:", middleName);
+let age = prompt("Введіть вік:");
+console.log("Вік:", age);
