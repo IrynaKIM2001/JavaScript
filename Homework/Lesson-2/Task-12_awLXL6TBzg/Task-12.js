1b33ac3fea4d510c87e999961193c75ad0ae7d0b
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-15 00:16:55 +0300

    HW2 Task_12

---
+++ Homework/Lesson-2/Task-12_awLXL6TBzg/Task-12.js
@@ -0,0 +1,44 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+const curse1 = coursesAndDurationArray[0];
+if(curse1.monthDuration >5) {
+    console.log("Супер");
+} else{
+    console.log("Погано")
+}
+const curse2 = coursesAndDurationArray[1];
+if(curse2.monthDuration >5) {
+    console.log("Супер");
+}else{
+    console.log("Погано")
+}
+const curse3 = coursesAndDurationArray[2];
+if(curse3.monthDuration >5) {
+    console.log("Супер");
+}else{
+    console.log("Погано")
+}
+if(coursesAndDurationArray[3].monthDuration >5) {
+    console.log("Супер");
+}else{
+    console.log("Погано")
+}
+if(coursesAndDurationArray[4].monthDuration >5) {
+    console.log("Супер");
+}else{
+    console.log("Погано")
+}
+if(coursesAndDurationArray[5].monthDuration >5) {
+    console.log("Супер");
+}else{
+    console.log("Погано")
+}
+
+
+
