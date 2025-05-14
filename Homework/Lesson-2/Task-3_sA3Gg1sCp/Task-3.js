9352a647626e8fb5b6f736a73ee333077ca5c3c6
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-14 21:48:59 +0300

    HW2 Task_3

---
+++ Homework/Lesson-2/Task-3_sA3Gg1sCp/Task-3.js
@@ -0,0 +1,29 @@
+let book1= {
+    title:'abc',
+    pageCount: 262,
+    genre:'fiction',
+    authors:[
+        {name:"William", age:29},
+        {name:"Stefan", age:56}
+    ]
+}
+let book2= {
+    title:'sds',
+    pageCount: 532,
+    genre:'non fiction',
+    authors:[
+        {name:"Petro", age:52},
+        {name:"Victor", age:41}
+    ]
+}
+let book3= {
+    title:'skh',
+    pageCount: 325,
+    genre:'fiction',
+    authors:[
+        {name:"Petro", age:42},
+        {name:"Stefan", age:46}
+    ]
+}
+
+
