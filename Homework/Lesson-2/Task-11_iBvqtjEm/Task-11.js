3750c39a5acd10f61256c81d7ff85f9c6eff416b
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-14 23:51:34 +0300

    HW2 Task_11

---
+++ Homework/Lesson-2/Task-11_iBvqtjEm/Task-11.js
@@ -0,0 +1,11 @@
+let x=0;
+if (x === 0 || x === null || x === undefined || x === "" || x === NaN) {
+    x='default';
+}
+console.log(x);
+
+ let y = null;
+if(!y){
+    y='default';
+}
+console.log(y);
