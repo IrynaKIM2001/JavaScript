2da43b4990f18ccab709b7cf1494b47543faddb6
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-14 21:30:28 +0300

    HW2 Task_2

---
+++ Homework/Lesson-2/Task-2_LARqoUj5I/Task-2.js
@@ -0,0 +1,3 @@
+let book1 = {title: 'Harry Potter', pageCount: 352, genre:'non fiction'}
+let book2 = {title: 'Night road', pageCount: 261, genre:'fiction'}
+let book3 = {title: 'The windows promise', pageCount: 54, genre:'drama'}
