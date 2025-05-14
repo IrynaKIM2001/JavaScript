06d007044479c536c3a0ff087cb8b7aa13fa20d2
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-14 22:04:14 +0300

    HW2 Task_4

---
+++ Homework/Lesson-2/Task-4_jCHFnEbdmFd/Task-4.js
@@ -0,0 +1,28 @@
+let user1={name:'Lia',username:'lias', password:'fsdfsfsdf'}
+let user2={name:'Ali',username:'Alisa', password:'jgj'}
+let user3={name:'Fiter',username:'Fit', password:'cvb'}
+let user4={name:'Likert',username:'Liker', password:'ljkljk'}
+
+let users=[
+    {name:'Stefan', username:'stef', password:'dkfskfj'},
+    user1,
+    {name:'Petro', username: 'petro', password:'sfsffff'},
+    user2,
+    user3,
+    {name:'William', username: 'william', password:'sfsdf'},
+    {name:'Andi', username: 'andi', password:'ghjjk'},
+    {name:'Taras', username: 'taras', password:'ggdgj'},
+    {name:'Kiril', username: 'kiril', password:'gsgdg'},
+    user4
+]
+console.log(users[0].password)
+console.log(users[1].password)
+console.log(users[2].password)
+console.log(users[3].password)
+console.log(users[4].password)
+console.log(users[5].password)
+console.log(users[6].password)
+console.log(users[7].password)
+console.log(users[8].password)
+console.log(users[9].password)
+
