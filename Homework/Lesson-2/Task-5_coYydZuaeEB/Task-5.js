0b1bcb078c880bdcd9f7ca46bef49dbbddda31b3
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-14 22:21:31 +0300

    HW2 Task_5

---
+++ Homework/Lesson-2/Task-5_coYydZuaeEB/Task-5.js
@@ -0,0 +1,9 @@
+let Terms=[
+    {day:"monday", morningTerm: 12, dayTerm:16, nightTerm:11},
+    {day:"tuesday", morningTerm: 22, dayTerm:19, nightTerm:14},
+    {day:"wednesday", morningTerm: 13, dayTerm:16, nightTerm:13},
+    {day:"thursday", morningTerm: 15, dayTerm:17, nightTerm:21},
+    {day:"friday", morningTerm: 16, dayTerm:35, nightTerm:22},
+    {day:"saturday", morningTerm: 14, dayTerm:25, nightTerm:13},
+    {day:"sunday", morningTerm: 18, dayTerm:13, nightTerm:20}
+]
