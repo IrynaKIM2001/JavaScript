80c1757dd31189130463550401e84d945573b8f8
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-14 22:58:10 +0300

    HW2 Task_6

---
+++ Homework/Lesson-2/Task-6_bAUsaq6LI/Task-6.js
@@ -0,0 +1,24 @@
+let x = 1
+if(x!==0){
+    console.log('Вірно');
+} else{
+    console.log('Невірно')
+}
+let x1 = 0
+if(x1!==0){
+    console.log('Вірно');
+} else{
+    console.log('Невірно')
+}
+let x2 = -3
+if(x2!==0){
+    console.log('Вірно');
+} else{
+    console.log('Невірно')
+}
+let x3 = +prompt('enter number: 1, 0 or -3')
+if(x3!==0){
+    console.log('Вірно');
+} else{
+    console.log('Невірно')
+}
