632f9734728b5792f080388c8879423fe72a0322
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-23 23:44:48 +0300

    ADD HW3 Task_5

---
+++ Homework/Lesson-3 Additional HW/Task-5_GamKju89ob/task-5.js
@@ -0,0 +1,3 @@
+// – Створити масив з 10 строкових елементів. Вивести в консоль всі його елементи в циклі.
+let arr = ['sdf','sfdsdf','sfsd','sfd','gdfg','dgdfh','hrer','cvb','gfhgf','ghfgh']
+console.log(arr)
