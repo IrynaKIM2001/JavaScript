831b81a2397b9529839a99b16e26c9ba2f5e87bc
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-23 23:48:17 +0300

    ADD HW3 Task_6

---
+++ Homework/Lesson-3 Additional HW/Task-6_Bm76xmg/task-6.js
@@ -0,0 +1,3 @@
+// Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+let arr= ['sdfsd', true, false, 'dfgdfg',23,2535, null, 5622, 'dfgdfg','dgfgdfg']
+console.log(arr)
