4b38f0987fe5d6e093ccd6f8cada35a948918088
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-23 23:54:34 +0300

    ADD HW3 Task_7

---
+++ Homework/Lesson-3 Additional HW/Task-7_u3vmD0YJXh/task-7.js
@@ -0,0 +1,6 @@
+// – Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+let arr= ['sdfsd', true, false, 'dfgdfg',23,2535, null, 5622, 'dfgdfg','dgfgdfg']
+for (let i = 0; i < arr.length; i++) {
+if (typeof arr[i]==='boolean'){
+    console.log(arr[i])
+}}
