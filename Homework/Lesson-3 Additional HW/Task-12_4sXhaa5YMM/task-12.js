59b1813c2f84d9ce3fe64a06f6074b5b3d344cc9
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-24 00:26:03 +0300

    ADD HW3 Task_12

---
+++ Homework/Lesson-3 Additional HW/Task-12_4sXhaa5YMM/task-12.js
@@ -0,0 +1,5 @@
+// – Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+for (let i=0; i<100;i++){
+    console.log(i+1);
+    document.write(` ${i+1} <br>`);
+}
