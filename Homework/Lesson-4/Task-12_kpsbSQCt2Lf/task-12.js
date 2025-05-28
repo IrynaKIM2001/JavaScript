41373ca713560d4417d8cda274f192879dc66642
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-28 23:57:57 +0300

    HW4 Task_12

---
+++ Homework/Lesson-4/Task-12_kpsbSQCt2Lf/task-12.js
@@ -0,0 +1,15 @@
+// – створити функцію swap(arr,index1,index2). Функція міняє місцями значення у відповідних індексах
+//
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+
+function swap(arr, I1,I2) {
+    if (I1 < arr.length && I2 < arr.length)
+    {
+    let temp = arr[I1];
+    arr[I1] = arr[I2];
+    arr[I2] = temp;
+    return arr;
+    }
+    return '!!!!!!!'
+}
+console.log(swap([11, 22, 33, 44], 0, 1));
