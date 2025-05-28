5f3d879ff0651e2d5499a5dde3a0865a0e6968d8
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-28 22:02:30 +0300

    HW4 Task_1

---
+++ Homework/Lesson-4/Task-1_I2XsG6f/Task-1.js
@@ -0,0 +1,8 @@
+// створити функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б
+
+function square (a,b){
+    let result=a*b;
+    console.log(result);
+    return result;
+}
+square(2,3)
