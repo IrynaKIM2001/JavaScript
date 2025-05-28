b9a9714ff86ee376d9cb1eb3176c988453993b4b
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-28 22:14:02 +0300

    HW4 Task_2

---
+++ Homework/Lesson-4/Task-2_ETGAxbEn8l/task-2.js
@@ -0,0 +1,8 @@
+// – створити функцію, яка обчислює та повертає площу кола з радіусом r
+
+function square_circle (Radius) {
+    let result = Math.PI*Radius*Radius;
+    console.log(result);
+    return result;
+}
+square_circle (4)
