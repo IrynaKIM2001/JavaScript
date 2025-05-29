2a5ec3a0624e4b6e5475521d165f0505d189a169
IrynaKIM2001 <ira2001tu@gmail.com>
2025-05-29 22:24:59 +0300

    HW4 Task_13

--- Homework/Lesson-4/Task-13_mkGDenYnNjn/Task-13.js
+++ Homework/Lesson-4/Task-13_mkGDenYnNjn/Task-13.js
@@ -3,7 +3,13 @@
 // Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
 
 function exchange(sumUAH, currencyValues, exchangeCurrency) {
-    let currency = currencyValues.find(item => item.currency === exchangeCurrency);
+    let currency = null;
+    for (let i = 0; i < currencyValues.length; i++) {
+        if (currencyValues[i].currency === exchangeCurrency) {
+            currency = currencyValues[i];
+            break;
+        }
+    }
     if (!currency) {
         return `Currency ${exchangeCurrency} not found`;
     }
