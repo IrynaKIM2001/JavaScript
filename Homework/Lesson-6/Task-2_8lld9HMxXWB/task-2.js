64bc007015c9c2dfc32e9878f120c091a603b3ef
IrynaKIM2001 <ira2001tu@gmail.com>
2025-06-04 21:04:45 +0300

    HW6 Task_2

---
+++ Homework/Lesson-6/Task-2_8lld9HMxXWB/task-2.js
@@ -0,0 +1,8 @@
+// – Перевести до великого регістру наступні стрінгові значеннz
+//       ‘hello world’, ‘lorem ipsum’, ‘javascript is cool’
+let i1 =  'hello world';
+console.log(i1.toUpperCase());
+let i2 = 'lorem ipsum';
+console.log(i2.toUpperCase());
+let i3 = 'javascript is cool';
+console.log(i3.toUpperCase());
