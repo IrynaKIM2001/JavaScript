af11e53c0cc8b3689ffb2610a600dacc278d2b95
IrynaKIM2001 <ira2001tu@gmail.com>
2025-06-04 21:19:46 +0300

    HW6 Task_4

--- Homework/Lesson-6/Task-3_ClDsAm7xba7/task-3.js
+++ Homework/Lesson-6/Task-3_ClDsAm7xba7/task-3.js
@@ -1,8 +1,8 @@
 // – Перевести до нижнього регістру наступні стрінгові значення
 //       ‘HELLO WORLD’, ‘LOREM IPSUM’, ‘JAVASCRIPT IS COOL’
-let i1 =  'hello world';
+let i1 =  'HELLO WORLD';
 console.log(i1.toLowerCase());
-let i2 = 'lorem ipsum';
+let i2 = 'LOREM IPSUM';
 console.log(i2.toLowerCase());
-let i3 = 'javascript is cool';
+let i3 = 'JAVASCRIPT IS COOL';
 console.log(i3.toLowerCase());
